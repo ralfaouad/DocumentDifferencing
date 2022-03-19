76af6f54fea5cd614612d378af618c2ad4a901d2
Ralph Aouad <aouadralph@outlook.com>
2022-03-19 23:24:25 +0200

    Merge branch 'master' of https://github.com/ralfaouad/DocumentDifferencing

--- Main/main.py
+++ Main/main.py
@@ -1,13 +1,13 @@
 import math
 import numpy as np
-from processing import preprocessing
-from save import get_matrices, save_matrix
+from processing import *
+from save import *
 from utils import *
 import xml.etree.ElementTree as ET
 
 
-treeA = preprocessing(ET.parse("treeA.xml").getroot())
-treeB = preprocessing(ET.parse("treeB.xml").getroot())
+treeA = preprocessing(ET.parse("C:/Users/ralf/Desktop/DocumentDifferencing/Main/treeA.xml").getroot())
+treeB = preprocessing(ET.parse("C:/Users/ralf/Desktop/DocumentDifferencing/Main/treeB.xml").getroot())
 
 
 def TED(A,B):
@@ -27,14 +27,18 @@ def TED(A,B):
         listB.append(subB)
     # print("listB: ",listB)
 
-    Dist = np.zeros(shape=(M + 1, N + 1))
+    Dist = np.zeros(shape = (M + 1, N + 1))
+    ES = [[0 for i in range(N+1)] for j in range(M+1)]
     Dist[0][0] = cost_upd(A,B)
+    ES[0][0] = str(A.tag) + ":" + str(B.tag)
 
     for i in range(1,Dist.shape[0]):
         Dist[i][0] = Dist[i-1][0] + cost_del_tree(listA[i-1],B)
+        ES[i][0] = str(listA[i-1].tag) + ":" + str(B.tag)
     
     for j in range(1,Dist.shape[1]):
         Dist[0][j] = Dist[0][j-1] + cost_ins_tree(listB[j-1],A)
+        ES[0][j] = str(listB[j-1].tag) + ":" + str(A.tag)
 
     for i in range(1,Dist.shape[0]):
         for j in range(1,Dist.shape[1]):
@@ -42,17 +46,18 @@ def TED(A,B):
             delete = Dist[i-1][j]+cost_del_tree(listA[i-1],B)
             insert = Dist[i][j-1]+cost_ins_tree(listB[j-1],A)
             Dist[i][j] = min(insert,delete,update)
-            if Dist[i][j] == insert and cost_ins_tree(listB[j-1],A) != 0: 
-                script.append("ins("+str(listB[j-1])+")")
-            elif Dist[i][j] == delete and cost_del_tree(listA[i-1],B) != 0:
-                script.append("del("+str(listA[i-1])+")")
-            else:
-                if TED(listA[i-1],B[j-1])!= 0:
-                    script.append("ted("+str(listA[i-1])+", "+str(listB[j-1])+")")
-
+            # if Dist[i][j] == insert and cost_ins_tree(listB[j-1],A) != 0: 
+            #     script.append("ins("+str(listB[j-1])+")")
+            # elif Dist[i][j] == delete and cost_del_tree(listA[i-1],B) != 0:
+            #     script.append("del("+str(listA[i-1])+")")
+            # else:
+            #     if TED(listA[i-1],B[j-1])!= 0:
+            #         script.append("ted("+str(listA[i-1])+", "+str(listB[j-1])+")")
     print("DIST MATRIX\n",Dist)
     save_matrix(Dist)
-    print(script)
+    # save_script(ES)
+    print(ES)
+    # print(script)
     return(Dist[M][N])
 
 # def ES(matrices,index=0):
