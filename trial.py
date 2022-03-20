867a6d2f2d63b82bb57f8d2c2ca04f085e417db2
KarenEstephan <estephankaren@yahoo.com>
2022-03-21 00:16:02 +0200

    gui 2.0

--- trial.py
+++ trial.py
@@ -13,7 +13,7 @@ from pyotp import Any
 root=Tk()
 root.title("XML Differencing tool")
 root.iconbitmap('hnet.com-image.ico')
-root['bg']='#ffffff'
+root['bg']='#cecece'
 
 
 
@@ -58,33 +58,33 @@ def compute():
     label=""
 
 
-b1=Button(root,text="DOC A",bg= "#4db6ac",fg="#ffffff",height=3,width=60,font=('Times',13,'bold'))
+b1=Button(root,text="DOC_A",bg= "#4db6ac",fg="#ffffff",height=3,width=60,font=('Times',13,'bold'))
 b1.place(x=100,y=70)
 
 
-b2=Button(root,text="DOC B",bg="#4db6ac",fg="#ffffff",height=3,width=60,font=('Times',13,'bold'))
+b2=Button(root,text="DOC_B",bg="#4db6ac",fg="#ffffff",height=3,width=60,font=('Times',13,'bold'))
 b2.place(x=100,y=170)
 
-b3=Button(root,text="ES IS:              ",bg="#0b4a44",fg="#ffffff",height=6,width=60,command=get_ES,font=('Times',13,'bold'))
+b3=Button(root,text="ES IS:              ",bg="#0b4a44",fg="#ffffff",height=4,width=40,command=get_ES,font=('Times',13,'bold'))
 b3.place(x=900,y=350)
 
-b4=Button(root,text="SIMILARITY  IS :                   ",bg="#0b4a44",fg="#ffffff",height=6,width=60,command=get_similarity,font=('Times',13,'bold'))
-b4.place(x=200,y=350)
+b4=Button(root,text="SIMILARITY  IS :                   ",bg="#0b4a44",fg="#ffffff",height=4,width=40,command=get_similarity,font=('Times',13,'bold'))
+b4.place(x=350,y=350)
 
-b5=Button(root,text="GET PATCHED FILE",bg="#16645c",fg="#ffffff",height=6,width=120,command=get_Patched,font=('Times',13,'bold'))
-b5.place(x=250,y=500)
+b5=Button(root,text="GET PATCHED FILE",bg="#16645c",fg="#ffffff",height=4,width=70,command=get_Patched,font=('Times',13,'bold'))
+b5.place(x=440,y=500)
 
-b6=Button(root,text="GET REVERSE PATCHED",bg="#16645c",fg="#ffffff",height=6,width=120,command=get_Reverse_Patched,font=('Times',13,'bold'))
-b6.place(x=250,y=650)
+b6=Button(root,text="GET REVERSE PATCHED",bg="#16645c",fg="#ffffff",height=4,width=70,command=get_Reverse_Patched,font=('Times',13,'bold'))
+b6.place(x=440,y=650)
 
 browse1=Button(root,text="Browse XML File",command=browse1)
 browse1.place(x=720,y=70,height=70,width=100)
 
 browse2=Button(root,text="Browse XML File",command=browse2)
-browse2.place(x=720,y=180,height=60,width=100)
+browse2.place(x=720,y=170,height=70,width=100)
 
 compute=Button(root,text="Compute",command=compute)
-compute.place(x=1540,y=270)
+compute.place(x=1500,y=270,height=40,width=100)
 
 
 
